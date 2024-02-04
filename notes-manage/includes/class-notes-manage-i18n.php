bdb0cfed3c134e1b42ac0911f9e6babc28c868bc
maha-alii <155061843+maha-alii@users.noreply.github.com>
2024-02-04 22:02:01 +0500

    Add files via upload
    added files

---
+++ notes-manage/includes/class-notes-manage-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://github.com/maha-alii
+ * @since      1.0.0
+ *
+ * @package    Notes_Manage
+ * @subpackage Notes_Manage/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Notes_Manage
+ * @subpackage Notes_Manage/includes
+ * @author     Maha Ali <maha@gmail.com>
+ */
+class Notes_Manage_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'notes-manage',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
